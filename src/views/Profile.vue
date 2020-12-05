fa65068056aaf017f851dcf2c308c8fc9ea039cd
LucasRAraujo <36748211+LucasRAraujo@users.noreply.github.com>
2020-12-04 21:35:21 -0300

    Create Profile.vue

---
+++ src/views/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Bem vindo(a) {{ user.data.displayName }} !</h1>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    name: 'Profile',
+    computed: {
+        ...mapGetters(['user'])
+    }
+}
+</script>
